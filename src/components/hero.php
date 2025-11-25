809a71c06a9911357a773ebdc2bc65075e24e1ce
ca797-rgb <ca797@scarletmail.rutgers.edu>
2025-11-25 14:42:42 -0500

    Update hero.php

--- src/components/hero.php
+++ src/components/hero.php
@@ -1,4 +1,18 @@
 <section class="hero-container">
-     <h1>Pear</h1>
+       <?php
+	
+<section class="hero">
+  <div class="hero-content">
+    <h1><?php echo $name; ?></h1>
+    <p class="price"><?php echo $price; ?></p>
+    <p class="description"><?php echo $description; ?></p>
+    </div>
+    <div class="hero-device">
+      <img src="<?php echo $image_url; ?>" alt="<?php echo $name; ?>">
+    </div>
+  </div>
+</section>
+?>
+
    
 </section>
