ceda00ac407dd4c00751c12efa00f5befa616b9e
andy8O <159593601+andy8O@users.noreply.github.com>
2025-12-03 16:49:00 -0500

    Update header.php

--- src/components/header.php
+++ src/components/header.php
@@ -1,11 +1,16 @@
+
 <header>
+     <div class="header-container">
+        <div class="logo">iPear</div>
+            <a href="/index.php">Home</a>
+            <a href="/index.php#product-category-list">Latest Products</a>
 
-<a href="/index.php" style="color: white !important;  ">Home</a>
-<a href="/index.php#product-category-list" style="color: white !important;  ">Latest Products</a>
+       
 
-
-<a href="/account.php" style="color: white !important;  ">Account</a>
-<a href="/checkout.php" style="color: white !important;  ">Checkout</a>
-<a href="/logout.php" style="color: white !important;  ">Log out</a>
-
+            <a href="/account.php">Account</a>
+            <a href="/logout.php">Log out</a>
+            <a href="/checkout.php">Checkout</a>
+            <a href="/cart.php">🧺</a>
+         
+    </div>
 </header>
